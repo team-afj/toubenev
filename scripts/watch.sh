0707e2266369d28f3efa4363cb6f560069c2a728
Ulysse Gérard <thevoodoos@gmail.com>
2025-10-12 14:11:25 +0200

    Update watch script

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -4,7 +4,7 @@
 
 fswatch -o toubenev.exe www/** www -l 1 | xargs -L1 bash -c \
 	"killall toubenev.exe || true; \
-	(CAQTI_DEBUG_PARAM=true ./toubenev.exe || export TBN_PID=$! || true) &"
+	(echo "" && CAQTI_DEBUG_PARAM=true ./toubenev.exe || true) &"
 
 echo "Killing toubenev"
 killall toubenev.exe
