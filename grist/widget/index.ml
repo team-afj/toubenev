422c7166d23f1401fbc856230f286de1fd56a033
Ulysse Gérard <thevoodoos@gmail.com>
2026-04-17 15:39:38 +0200

    Fast feedback for dev

--- grist/widget/index.ml
+++ grist/widget/index.ml
@@ -14,40 +14,56 @@ let fetch table_id =
   let+ result = Doc_API.fetch_table ~table_id in
   Data.Row_records.by_row result
 
-let _ =
-  let* infos = fetch infos_tbl_id in
-  let* options = fetch options_tbl_id in
-  let* places = fetch places_tbl_id in
-  let* task_types = fetch task_types_tbl_id in
-  let* time_specs = fetch time_slots_tbl_id in
-  let* volunteers = fetch volunteers_tbl_id in
-  let* quests = fetch quests_tbl_id in
-
-  let data =
-    Jv.obj
-      [|
-        ("infos", infos);
-        ("options", options);
-        ("places", places);
-        ("task_types", task_types);
-        ("time_specs", time_specs);
-        ("volunteers", volunteers);
-        ("quests", quests);
-      |]
-  in
-  Console.log [ "POUET POUET" ];
-  Console.log [ data ];
-  let+ res =
-    let open Brr_io.Fetch in
-    let body = Body.of_jstr (Json.encode data) in
-
-    Console.log [ "BODY"; body ];
-    let method' = Jstr.v "PUT" in
-    let url = Jstr.v "/grist/data" in
-    let headers =
-      Headers.of_assoc [ (Jstr.v "Content-Type", Jstr.v "application/json") ]
+let sat =
+  let last_data = ref None in
+  fun () ->
+    ignore
+    @@
+    let* infos = fetch infos_tbl_id in
+    let* options = fetch options_tbl_id in
+    let* places = fetch places_tbl_id in
+    let* task_types = fetch task_types_tbl_id in
+    let* time_specs = fetch time_slots_tbl_id in
+    let* volunteers = fetch volunteers_tbl_id in
+    let* quests = fetch quests_tbl_id in
+    let data =
+      Jv.obj
+        [|
+          ("infos", infos);
+          ("options", options);
+          ("places", places);
+          ("task_types", task_types);
+          ("time_specs", time_specs);
+          ("volunteers", volunteers);
+          ("quests", quests);
+        |]
     in
-    let init = Request.init ~body ~method' ~headers () in
-    Brr_io.Fetch.url ~init url
-  in
-  Console.log [ res ]
+    match !last_data with
+    | Some last when Jv.equal last data -> Fut.return (Ok ())
+    | _ ->
+        let () = last_data := Some data in
+        let+ res =
+          let open Brr_io.Fetch in
+          let body = Body.of_jstr (Json.encode data) in
+
+          let method' = Jstr.v "PUT" in
+          let uri = Jstr.v "/grist/data" in
+          let headers =
+            Headers.of_assoc
+              [ (Jstr.v "Content-Type", Jstr.v "application/json") ]
+          in
+          let init = Request.init ~body ~method' ~headers () in
+          let* response = url ~init uri in
+          Response.as_body response |> Body.json
+        in
+        let elt = El.find_first_by_selector (Jstr.v "#readout") |> Option.get in
+        Console.log [ res ];
+        let j = Jv.get Jv.global "JSON" in
+        let str =
+          Jv.call j "stringify" [| res; Jv.null; Jv.of_int 2 |] |> Jv.to_jstr
+        in
+        El.set_children elt
+        @@ List.map ~f:(fun s -> El.pre [ El.txt s ])
+        @@ Jstr.cuts ~sep:(Jstr.v "\\n") str
+
+let _ = Brr.G.set_interval ~ms:2000 sat
--- server/grist.ml
+++ server/grist.ml
@@ -16,19 +16,27 @@ let handle_put_data (req : (Vif.Type.json, Grist_import.data) Request.t) _server
   let r =
     let open Result in
     Logs.debug (fun m -> m "BEFORE");
-    let* v = Request.of_json req in
+    let+ v = Request.of_json req in
     Logs.debug (fun m -> m "AFTER");
-    let+ s =
-      Jsont_bytesrw.encode_string Grist_import.data_jsont v
-      |> Rresult.R.(map_error msg)
-    in
     let planning = Grist_import.to_planning v in
     let status = Api.Planning.solve planning in
-    Logs.debug (fun m -> m "%s" s);
+    (* let+ s =
+      Jsont_bytesrw.encode_string Types.Planning.jsont planning
+      |> Rresult.R.(map_error msg)
+    in
+    Logs.debug (fun m -> m "%s" s); *)
     status
   in
   let status =
-    match r with Error (`Msg err) -> Format.sprintf "ERROR %s" err | Ok s -> s
+    match r with
+    | Error (`Msg err) ->
+        {
+          Api.Planning.status = Format.asprintf "ERROR %s" err;
+          solution = "";
+          sufficient_assumptions_for_infeasibility = "";
+          log = "";
+        }
+    | Ok s -> s
   in
-  let* () = Response.with_string req status in
+  let* () = Response.with_json req Api.Planning.answer_jsont status in
   Response.respond `OK
