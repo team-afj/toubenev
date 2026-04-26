01342308197a22460b137e6b34c215a475b6147b
Ulysse Gérard <thevoodoos@gmail.com>
2026-04-26 09:57:55 +0200

    Ad quests intervals in context

--- server/cp_model/context.ml
+++ server/cp_model/context.ml
@@ -8,6 +8,7 @@ type t = {
   options : Options.t;
   with_assumptions : bool;
   assignations : Volunteer.t -> Quest.t -> Sat.Var.t_bool;
+  intervals : Volunteer.t -> Quest.t -> Sat.interval_var;
   assignations_rev : int -> Sat.Var.t_bool * Volunteer.t * Quest.t;
   vs : Volunteers.t;
   qs : Quests.t;
@@ -22,7 +23,7 @@ let assignations m vs qs =
   let rev_tbl : (int, Sat.Var.t_bool * Volunteer.t * Quest.t) Hashtbl.t =
     Hashtbl.create size
   in
-  let by_uuid =
+  let by_uuid : (Sat.Var.t_bool * Sat.interval_var) Uuidm_map.t Uuidm_map.t =
     Volunteers.fold vs ~init:Uuidm_map.empty ~f:(fun acc (v : Volunteer.t) ->
         let quests =
           Quests.fold qs ~init:Uuidm_map.empty ~f:(fun acc (q : Quest.t) ->
@@ -33,15 +34,33 @@ let assignations m vs qs =
               let var = Sat.Var.new_bool m name in
               Hashtbl.add rev_tbl !c (var, v, q);
               incr c;
-              Uuidm_map.add q.id var acc)
+              let interval =
+                let open Lunar in
+                let datetime_to_minutes d =
+                  Datetime.to_duration d |> Duration.to_minutes
+                  |> Sat.LinearExpr.of_int
+                in
+                let start = datetime_to_minutes q.slot.start in
+                let end_ = datetime_to_minutes (Time_slot.end_ q.slot) in
+                let size = Sat.(end_ - start) in
+                let name =
+                  Format.sprintf "%i_interval_%s_does_%s" !c v.initial.name
+                    q.name
+                in
+                Sat.new_optional_interval_var m ~start ~size ~end_
+                  ~is_present:var name
+              in
+              Uuidm_map.add q.id (var, interval) acc)
         in
         Uuidm_map.add v.id quests acc)
   in
   let find =
    fun v q -> Uuidm_map.find v.Volunteer.id by_uuid |> Uuidm_map.find q.Quest.id
   in
+  let find_assignation v q = fst (find v q) in
+  let find_interval v q = snd (find v q) in
   let rev_find = fun i -> Hashtbl.find rev_tbl i in
-  (find, rev_find)
+  (find_assignation, find_interval, rev_find)
 
 let prepare ~with_assumptions model (data : Planning.t) =
   (* TODO: split quests, group friends and quests groups, etc *)
@@ -56,7 +75,7 @@ let prepare ~with_assumptions model (data : Planning.t) =
     |> Quests.of_list
   in
   let task_types = CCRAL.to_list data.task_types |> Task_type.Set.of_list in
-  let assignations, assignations_rev = assignations model vs qs in
+  let assignations, intervals, assignations_rev = assignations model vs qs in
   let for_all_quests f = Quests.iter qs ~f in
   let for_all_volunteers f = Volunteers.iter ~f vs in
   {
@@ -64,6 +83,7 @@ let prepare ~with_assumptions model (data : Planning.t) =
     with_assumptions;
     options = data.options;
     assignations;
+    intervals;
     assignations_rev;
     vs;
     qs;
