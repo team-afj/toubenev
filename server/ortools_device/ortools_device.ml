671819edf45aff649af77069fdc56801c6b83c5a
Ulysse Gérard <thevoodoos@gmail.com>
2026-05-14 11:55:52 +0200

    Wip: ortools device

--- server/ortools_device/ortools_device.ml
+++ server/ortools_device/ortools_device.ml
@@ -1,5 +1,40 @@
-type store = (string, unit) Hashtbl.t
+type response_queue =
+  (Ortools.Sat.Response.t, Ortools.Sat.Response.t option) Flux.Bqueue.t
+
+type t = {
+  tasks : (string, response_queue * unit Miou.t) Hashtbl.t;
+  orphans : unit Miou.orphans;
+}
+
+let new_optim t (p : Data_repr.Rich.Planning.t) =
+  let handle = new_random_uuid_v4 () |> Uuidm.to_string in
+  let queue =
+    (* TODO This can block if there is not enogh readers... an unbounded queue
+       would be a better fit. *)
+    Flux.Bqueue.(create with_close 1000)
+  in
+  let promise =
+    Miou.call ~orphans:t.orphans @@ fun () ->
+    let context = Cp_model.Model.make ~with_assumptions:false p in
+    let parameters =
+      Ortools.Sat_parameters.make_sat_parameters ~log_search_progress:false
+        ~num_workers:8l ()
+    in
+    let observer response = Flux.Bqueue.put queue response in
+    let response =
+      Ortools_solvers.Sat.solve ~observer ~parameters context.model
+    in
+    Flux.Bqueue.put queue response;
+    Flux.Bqueue.close queue
+  in
+  Hashtbl.add t.tasks handle (queue, promise);
+  handle
 
 let v =
-  let finally (_store : store) = (* empty the store *) () in
-  Vif.Device.v ~name:"ortools" ~finally [] @@ fun () -> Hashtbl.create 16
+  let finally (t : t) =
+    match Miou.care t.orphans with
+    | Some (Some promise) -> Miou.await_exn promise
+    | _ -> ()
+  in
+  Vif.Device.v ~name:"ortools" ~finally [] @@ fun () ->
+  { tasks = Hashtbl.create 16; orphans = Miou.orphans () }
