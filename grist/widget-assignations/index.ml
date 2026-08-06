301e8e7633691925450ca7c0d38687dd5a8779fc
Ulysse Gérard <thevoodoos@gmail.com>
2026-08-06 16:49:33 +0200

    First working assignations widget

--- grist/widget-assignations/index.ml
+++ grist/widget-assignations/index.ml
@@ -1,26 +1,49 @@
 open Brrer
 open Brr
 open Brr_lwd
+open Lwd_infix
 open! Fut.Result_syntax
 open! Lunar_jsont
 open! Data_repr
+open! Rich
+open Normal
 open Tables
 
-let on_record () =
-  (* For custom widgets, add a handler that will be called whenever the row with
-     the cursor changes. If the widget is correctly set this means rows from the
-     SOLUTIONS table. *)
-  let f =
-   fun obj ->
-    Console.log [ "TBN ASS DBG ON RECORD"; obj ];
-    let o =
-      (Jv.Int.get obj "id", Jv.Jstr.get obj "name", Jv.get obj "last_answer")
-    in
-    (* Lwd.set App_state.grist_solutions o; *)
-    Console.log [ "TBN DBG ON RECORD PARSED"; o ]
+(* This widget should be associated to the ASSIGNATION table and filtered by
+   SOLUTION *)
+
+module App_state = struct
+  let active_assignations :
+      (Rich.Planning.t * Api.data * Api.assignation list) option Lwd.var =
+    Lwd.var None
+end
+
+let resolve_assignation_jv (data : Api.data) ass =
+  Console.log [ "TBN ASS ASS "; ass ];
+  let quest_id = Jv.Jstr.get ass "ref" |> Jstr.to_string in
+  let volunteers_ids =
+    let jv = Jv.get ass "volunteers" in
+    if Jv.is_none jv then [] else Jv.to_list Jv.to_int jv
   in
-  let callback = Jv.callback ~arity:1 f in
-  Grist.on_record ~callback ()
+  let quest = Quests.find_by_id quest_id data.quests in
+  let by_id =
+    let tbl = Hashtbl.create 128 in
+    Volunteers.iter data.volunteers ~f:(fun v -> Hashtbl.add tbl v.id v);
+    tbl
+  in
+  let volunteers =
+    List.fold_left volunteers_ids ~init:Volunteers.empty ~f:(fun acc i ->
+        Console.log [ "TBN ASS DBG LK FOR V"; i ];
+        try
+          let v = Hashtbl.find by_id (string_of_int i) in
+          Console.log [ "TBN ASS FOUND "; v.name ];
+          Volunteers.add v acc
+        with err ->
+          Console.log [ "TBN ASS OUPS "; err ];
+          acc)
+  in
+  Console.log [ "TBN ASS FNISHE " ];
+  { Api.quest; volunteers }
 
 let on_records () =
   (* For custom widgets, add a handler that will be called whenever the selected
@@ -28,17 +51,47 @@ let on_records () =
      ASSIGNATIONS table. *)
   let f =
    fun v ->
-    let solutions = Solutions.ls () in
-    Console.log [ "TBN ASS DBG ON RECORDS"; v; solutions ]
+    Console.log [ "TBN ASS DBG ON RECORDS"; v ];
+    match Jv.to_jv_list v with
+    | [] -> Fut.ok ()
+    | hd :: _ as assignations ->
+        (* Solution should contain a ROW ID *)
+        let* solution =
+          let solution_id = Jv.(Int.get (get hd "solution") "rowId") in
+          Solutions.get_solution (solution_id - 1)
+        in
+        let+ data = Solutions.data_rich solution |> Fut.return in
+        let normal = Conv.normalize data in
+        (* Console.log
+          [
+            "TBN ASS DBG ALL V";
+            Volunteers.fold normal.volunteers ~init:"" ~f:(fun s v ->
+                s ^ " " ^ v.id ^ " " ^ v.name);
+          ]; *)
+        let assignations =
+          List.map assignations ~f:(resolve_assignation_jv normal)
+        in
+        Console.log [ "TBN ASS DBG ON RECORDS ASS3" ];
+        Lwd.set App_state.active_assignations
+          (Some (data, normal, assignations))
   in
   let callback = Jv.callback ~arity:1 f in
-  Grist.on_records ~callback ()
+  let options =
+    Jv.obj [| ("keepEncoded", Jv.false'); ("expandRefs", Jv.false') |]
+  in
+  Grist.on_records ~callback ~options ()
 
-let app = Elwd.div [ `P (El.txt' "todo") ]
+let app =
+  let content =
+    let$* state = Lwd.get App_state.active_assignations in
+    match state with
+    | None -> Lwd.return (El.nbsp ())
+    | Some (_, data, assignations) -> Infos.per_volunteer data assignations
+  in
+  Elwd.div [ `R content ]
 
 let _ =
   let on_load _ =
-    let () = on_record () in
     let () = on_records () in
     let root = El.find_first_by_selector (Jstr.v "main") |> Option.get in
     let app = Lwd.observe app in
