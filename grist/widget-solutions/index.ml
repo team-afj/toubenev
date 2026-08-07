59e8fa135d84c8b8c84d0615715c4814998237d4
Ulysse Gérard <thevoodoos@gmail.com>
2026-08-07 09:27:19 +0200

    widget-solutions

---
+++ grist/widget-solutions/index.ml
@@ -0,0 +1,71 @@
+open Brrer
+open Brr
+open Brr_lwd
+open Lwd_infix
+open! Fut.Result_syntax
+open! Lunar_jsont
+open! Data_repr
+open! Rich
+open! Normal
+open! Tables
+
+(* This widget should be associated to the ASSIGNATION table and filtered by
+   SOLUTION *)
+
+module App_state = struct
+  let active_solution : int Lwd.var = Lwd.var 0
+end
+(*
+let resolve_assignation_jv (data : Api.data) ass =
+  Console.log [ "TBN ASS ASS "; ass ];
+  let quest_id = Jv.Jstr.get ass "ref" |> Jstr.to_string in
+  let volunteers_ids =
+    let jv = Jv.get ass "volunteers" in
+    if Jv.is_none jv then [] else Jv.to_list Jv.to_int jv
+  in
+  let quest = Quests.find_by_id quest_id data.quests in
+  let by_id =
+    let tbl = Hashtbl.create 128 in
+    Volunteers.iter data.volunteers ~f:(fun v -> Hashtbl.add tbl v.id v);
+    tbl
+  in
+  let volunteers =
+    List.fold_left volunteers_ids ~init:Volunteers.empty ~f:(fun acc i ->
+        try
+          let v = Hashtbl.find by_id (string_of_int i) in
+          Volunteers.add v acc
+        with err ->
+          Console.error [ "TBN ASS OUPS "; err ];
+          acc)
+  in
+  { Api.quest; volunteers } *)
+
+let on_records () =
+  (* For custom widgets, add a handler that will be called whenever the selected
+     records change. If the widget is correctly set this means rows from the
+     ASSIGNATIONS table. *)
+  let f = fun v -> Console.log [ "TBN DBG Solutions records"; v ] in
+  let callback = Jv.callback ~arity:1 f in
+  let options =
+    Jv.obj [| ("keepEncoded", Jv.false'); ("expandRefs", Jv.false') |]
+  in
+  Grist.on_records ~callback ~options ()
+
+let app =
+  let content =
+    let$* state = Lwd.get App_state.active_solution in
+    Lwd.return (El.txt' (string_of_int state))
+  in
+  Elwd.div [ `R content ]
+
+let _ =
+  let on_load _ =
+    let () = on_records () in
+    let root = El.find_first_by_selector (Jstr.v "main") |> Option.get in
+    let app = Lwd.observe app in
+    let f _ = ignore @@ Lwd.quick_sample app in
+    let on_invalidate _ = ignore @@ G.request_animation_frame f in
+    El.append_children root [ Lwd.quick_sample app ];
+    Lwd.set_on_invalidate app on_invalidate
+  in
+  Ev.listen Ev.dom_content_loaded on_load (Window.as_target G.window)
--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       input: {
         main: resolve(import.meta.dirname, root, 'index.html'),
         plannings: resolve(import.meta.dirname, root, 'plannings.html'),
+        solution: resolve(import.meta.dirname, root, 'solutions/index.html'),
         assignations: resolve(import.meta.dirname, root, 'assignations/index.html'),
       },
     },
