6af347e64ca6448cc5bb8f8cc17b1b9e559d3f13
Ulysse Gérard <thevoodoos@gmail.com>
2026-08-09 21:39:18 +0200

    Hide 0-diff volunteers

--- grist/grist-common/diffs_table.ml
+++ grist/grist-common/diffs_table.ml
@@ -7,7 +7,10 @@ open Data_repr
 open Normal
 open Analysis
 
-let print_signed_int = Int.to_string
+let print_signed_int i =
+  let s = Int.to_string i in
+  (* if i = 0 then "±" ^ s else *)
+  if i > 0 then "+" ^ s else s
 
 let color_grad i =
   At.style (Jstr.v ("background-color: hwb(9 " ^ string_of_int i ^ "% 0%);"))
@@ -19,7 +22,7 @@ let make_red i =
   let i = i * 100 / max_red in
   color_grad (100 - i)
 
-let make_table (analysis : t) real_time =
+let make_table (analysis : t) real_time hide_zeros =
   let head =
     Date.Map.fold
       (fun date _ acc ->
@@ -46,7 +49,8 @@ let make_table (analysis : t) real_time =
             El.txt' (" (" ^ Duration.to_string expected_load ^ ")");
           ] )
     in
-    ( total_diff,
+    let is_zero = ref (total_diff = 0) in
+    let day_diffs =
       Date.Map.fold
         (fun _ facts acc ->
           let expected_load =
@@ -55,6 +59,7 @@ let make_table (analysis : t) real_time =
           let diff =
             Duration.(facts.actual_load - expected_load |> to_minutes)
           in
+          if diff <> 0 then is_zero := false;
           El.td
             ~at:[ make_red diff ]
             [
@@ -63,11 +68,15 @@ let make_table (analysis : t) real_time =
             ]
           :: acc)
         facts.daily [ total; name ]
-      |> List.rev |> El.tr )
+      |> List.rev |> El.tr
+    in
+    ((total_diff, day_diffs), !is_zero)
   in
   let volunteers =
     Volunteer.Map.fold
-      (fun v f acc -> volunteer v f :: acc)
+      (fun v f acc ->
+        let el, is_zero = volunteer v f in
+        if is_zero && hide_zeros then acc else el :: acc)
       analysis.volunteers []
     |> List.sort ~cmp:(fun (d1, _) (d2, _) -> Int.compare d2 d1)
     |> List.map ~f:snd
@@ -75,7 +84,7 @@ let make_table (analysis : t) real_time =
   El.table ~at:[ Pico_ui.At.overflow_auto ] (head :: volunteers)
 
 let make (analysis : t) =
-  let { element; desc = Check { state; _ } } =
+  let { element = check_kind; desc = Check { state; _ } } =
     Forms.Field_checkboxes.make_single
       {
         value = ();
@@ -92,7 +101,18 @@ let make (analysis : t) =
         state = true;
       }
   in
-  let table =
-    Lwd.map (Lwd.get state) ~f:(fun s -> make_table analysis (Option.is_some s))
+  let { element = check_zero; desc = Check { state = state_zero; _ } } =
+    Forms.Field_checkboxes.make_single
+      {
+        value = ();
+        id = "hide_zero_diff_switch";
+        name = "hide_zero_diff_switch";
+        label = (fun () -> [ `P (El.txt' "Masquer les bénévoles à ±0") ]);
+        state = true;
+      }
   in
-  Elwd.div [ `R element; `R table ]
+  let table =
+    Lwd.map2 (Lwd.get state) (Lwd.get state_zero) ~f:(fun s zero ->
+        make_table analysis (Option.is_some s) (Option.is_some zero))
+  in
+  Elwd.div [ `R check_kind; `R check_zero; `R table ]
