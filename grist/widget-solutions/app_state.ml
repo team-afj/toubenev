bb0a5367cf64cdb3e461f91cce0bfb68cf345dc5
Ulysse Gérard <thevoodoos@gmail.com>
2026-08-09 16:15:05 +0200

    wip solution widget

---
+++ grist/widget-solutions/app_state.ml
@@ -0,0 +1,27 @@
+let selected_in_grist : int Lwd.var = Lwd.var 1
+
+let solutions : (int * string * Jstr.t) Lwd_seq.t Lwd.var =
+  Lwd.var Lwd_seq.empty
+
+let sol_select =
+  let options =
+    Lwd.get solutions
+    |> Lwd_seq.map (fun (id, name, _) -> (string_of_int id, name))
+  in
+  let field_desc =
+    let default = "1" in
+    { Forms.Field.name = "sol_solutions_select"; default; label = [] }
+  in
+  Forms.Field_select.make field_desc options
+
+let selected_solution = Lwd.get sol_select.value
+
+type optimize_state = Not_ready | Ready of Tables.Solutions.t | Running
+
+let optimize_state : optimize_state Lwd.var = Lwd.var Not_ready
+
+type server_status = Offline | Working | Done
+
+let to_fr_slug = function Offline -> "⛓️‍💥" | Working -> "🤖" | Done -> "🔗"
+
+let server_status : server_status Lwd.var = Lwd.var Offline
--- grist/widget-solutions/index.ml
+++ grist/widget-solutions/index.ml
@@ -12,26 +12,6 @@ open! Tables
 (* This widget should be associated to the ASSIGNATION table and filtered by
    SOLUTION *)
 
-module App_state = struct
-  let selected_in_grist : int Lwd.var = Lwd.var 1
-
-  let solutions : (int * string * Jstr.t) Lwd_seq.t Lwd.var =
-    Lwd.var Lwd_seq.empty
-
-  let sol_select =
-    let options =
-      Lwd.get solutions
-      |> Lwd_seq.map (fun (id, name, _) -> (string_of_int id, name))
-    in
-    let field_desc =
-      let default = "1" in
-      { Forms.Field.name = "sol_solutions_select"; default; label = [] }
-    in
-    Forms.Field_select.make field_desc options
-
-  let selected_solution = Lwd.get sol_select.value
-end
-
 let grist_set_pointer id =
   let pos = Jv.obj [| ("rowId", Jv.of_int (int_of_string id)) |] in
   (* This requires ready({allowSelectBy:true}) *)
@@ -70,7 +50,171 @@ let delete ~solution =
   let* _ = Tables.Assignations.remove_assignations ~solution in
   Tables.Solutions.remove ~solution
 
+let solution_placeholder (quests : Normal.Quests.t) =
+  Normal.Quests.to_list_map
+    ~f:(fun quest -> { Api.quest; volunteers = quest.assigned_volunteers })
+    quests
+
+let unwind_planning () =
+  Fut.bind (Data.fetch_all ()) @@ function
+  | Error jv_err ->
+      Console.error [ "DBG"; "Decoding error: "; jv_err ];
+      let err =
+        Jv.Error.message jv_err |> Jstr.to_string
+        |> String.replace ~sub:"[0m" ~by:"\""
+        |> String.replace ~sub:"[1m" ~by:"\""
+      in
+      let error =
+        [
+          El.p
+            [
+              El.txt'
+                "Une erreur est survenue lors de la lecture des données. Cela \
+                 signifie probablement que le type d'une colonne n'est pas \
+                 respéctée ou qu'une information nécéssaire est manquante.";
+            ];
+          El.pre [ El.txt' err ];
+        ]
+      in
+      Pico_ui.Modal.one_shot ~title:"Oups !" error;
+      Fut.error jv_err
+  | Ok data ->
+      let _id_map, planning = Grist_import.to_planning data in
+      let normalized_planning = Conv.normalize planning in
+      let initial_answer =
+        {
+          Api.dummy_answer with
+          solution = solution_placeholder normalized_planning.quests;
+          diagnostics = normalized_planning.diagnostics;
+        }
+      in
+      let analysis =
+        Shared.Analysis.of_planning planning initial_answer normalized_planning
+      in
+      let state =
+        {
+          Tables.Solutions.data;
+          data_rich = planning;
+          answer = initial_answer;
+          analysis;
+        }
+      in
+      (* let* () = Assignations.remove_assignations ~solution:1 in
+      let* () = Solutions.upsert_solution_1 state in
+      let* () = Assignations.insert_assignations initial_assignations in *)
+      Fut.ok (state, normalized_planning)
+
+let server_sat_check_query data =
+  let open Brr_io.Fetch in
+  let* data = Fut.return @@ Jsont_brr.encode Grist_import.data_jsont data in
+  let body = Body.of_jstr data in
+  Console.debug [ "TBN"; "Querying server" ];
+  let method' = Jstr.v "PUT" in
+  let uri = Jstr.v "https://localhost:5173/check-data" in
+  let headers =
+    Headers.of_assoc [ (Jstr.v "Content-Type", Jstr.v "application/json") ]
+  in
+  let init = Request.init ~body ~method' ~headers () in
+  let open Fut.Syntax in
+  let () = Lwd.set App_state.server_status Working in
+  let* response = url ~init uri in
+  match response with
+  | Error err ->
+      Console.info [ "TBN"; "Error querying server" ];
+      let* _ = Titles.update_prefixes "⛓️‍💥" in
+      let () = Lwd.set App_state.server_status Offline in
+      Fut.error err
+  | Ok resp ->
+      Console.info [ "TBN"; "Got server response" ];
+      let () = Lwd.set App_state.server_status Done in
+      let open Fut.Result_syntax in
+      let* body = Response.as_body resp |> Body.json in
+      Fut.return @@ Jsont_brr.decode_jv Data_repr.Api.answer_jsont body
+
+let rev_append_diags diags (answer : Api.answer) =
+  { answer with diagnostics = List.rev_append diags answer.diagnostics }
+
+let server_sat_check (state : Solutions.t) normalized_planning =
+  let open Fut.Syntax in
+  let* response = server_sat_check_query state.data in
+  let initial_answer = state.answer in
+  let planning = state.data_rich in
+  let open Fut.Result_syntax in
+  match response with
+  | Error jv ->
+      let answer =
+        rev_append_diags
+          [ (Error, Jv.Error.message jv |> Jstr.to_string) ]
+          initial_answer
+      in
+      let state = { state with answer } in
+      (* Solutions.upsert_solution_1 state *)
+      Fut.ok state
+  | Ok answer ->
+      let* answer =
+        match answer.status with
+        | Feasible | Optimal ->
+            let+ () = Titles.update_prefixes "🟢" in
+            answer
+        | Unknown | ModelInvalid | Infeasible ->
+            let+ () = Titles.update_prefixes "🔴" in
+            { answer with solution = initial_answer.solution }
+      in
+      let answer = rev_append_diags initial_answer.diagnostics answer in
+      Console.debug [ "TBN"; "Perform analysis on assignations." ];
+      let analysis =
+        Shared.Analysis.of_planning planning answer normalized_planning
+      in
+      let state = { state with answer; analysis } in
+      let () =
+        match answer.status with
+        | Feasible | Optimal -> Lwd.set App_state.optimize_state (Ready state)
+        | Api.Unknown | Api.ModelInvalid | Api.Infeasible ->
+            Lwd.set App_state.optimize_state Not_ready
+      in
+      Fut.ok state
+(* Solutions.upsert_solution_1 state *)
+(* Fut.ok @@ Lwd.set App_state.last_answer (Some state) *)
+
+let update_solution state =
+  let* () = Assignations.remove_assignations ~solution:1 in
+  let* () = Solutions.upsert_solution_1 state in
+  let assignations =
+    List.map state.answer.solution ~f:(Grist_import.Assignation.v ~solution:1)
+  in
+  Assignations.insert_assignations assignations
+
 let app =
+  let controls =
+    let in_progress = Lwd.var false in
+    let check_btn =
+      let disabled =
+        Lwd.map (Lwd.get in_progress) ~f:(function
+          | true -> At.disabled
+          | false -> At.void)
+      in
+      let ev =
+        Elwd.handler Ev.click (fun _ ->
+            Lwd.set in_progress true;
+            let fut =
+              let* new_state, normalized_planning = unwind_planning () in
+              let* new_state = server_sat_check new_state normalized_planning in
+              update_solution new_state
+            in
+            Fut.await fut (fun _ -> Lwd.set in_progress false))
+      in
+      Elwd.button
+        ~at:[ `R disabled ]
+        ~ev:[ `P ev ]
+        [ `P (El.txt' "1. Déplier les quêtes et vérifier la faisabilité") ]
+    in
+    let btns =
+      Elwd.fieldset
+        ~at:[ `P (At.v (Jstr.v "role") (Jstr.v "group")) ]
+        [ `R check_btn ]
+    in
+    Pico_ui.Elwd.section [ `R btns ]
+  in
   let solution_manager =
     let focus_btn =
       let disabled =
@@ -133,7 +277,7 @@ let app =
         `R App_state.sol_select.field; `R focus_btn; `R copy_btn; `R delete_btn;
       ]
   in
-  Elwd.div [ `R solution_manager ]
+  Elwd.div [ `R controls; `R solution_manager ]
 
 let decode_solution_jv sol =
   let sol_id = Jv.Int.get sol "id" in
