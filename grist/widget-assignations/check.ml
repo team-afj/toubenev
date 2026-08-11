a530707d5fa0bb99eb8a2e425df892bce1f8b119
Ulysse Gérard <thevoodoos@gmail.com>
2026-08-11 13:05:11 +0200

    dedup

--- grist/widget-assignations/check.ml
+++ grist/widget-assignations/check.ml
@@ -21,8 +21,12 @@ let check_q_filled { Api.quest; volunteers } =
   | n when n > 0 -> Error (`Too_many_volunteers (quest, n))
   | n -> Error (`Too_few_volunteers (quest, n))
 
-let check_can_do (infos : Event_infos.t) (checks : Static_analysis.with_cache)
-    (assignations : Api.assignation list) { Api.quest; volunteers } =
+let check_can_do dedup (infos : Event_infos.t)
+    (checks : Static_analysis.with_cache) (assignations : Api.assignation list)
+    { Api.quest; volunteers } =
+  let sort_ids q q' =
+    if String.(q.Quest.id <= q'.Quest.id) then (q.id, q'.id) else (q'.id, q.id)
+  in
   let overlapping_assignations =
     List.filter assignations ~f:(fun { Api.quest = q'; _ } ->
         (not (String.equal quest.id q'.id)) && Quest.overlaps infos quest q')
@@ -34,15 +38,24 @@ let check_can_do (infos : Event_infos.t) (checks : Static_analysis.with_cache)
       can_do
       :: List.map overlapping_assignations
            ~f:(fun { Api.quest = q'; volunteers } ->
-             if Volunteers.mem v volunteers then
-               Error (`Multiple_assignements (v, quest, q'))
+             let key = sort_ids quest q' in
+             if Hashtbl.mem dedup key then Ok ()
+             else if Volunteers.mem v volunteers then (
+               Hashtbl.add dedup key ();
+               Error (`Multiple_assignements (v, quest, q')))
              else Ok ()))
   |> List.flatten
+
+let check_can_do_init () =
+  let dedup = Hashtbl.create 64 in
+  fun infos checks assignations ass ->
+    check_can_do dedup infos checks assignations ass
 
 let filter_errors l =
   List.filter_map l ~f:(function Ok () -> None | Error err -> Some err)
 
 let assignations infos checks (l : Api.assignation list) =
+  let check_can_do = check_can_do_init () in
   List.flat_map l ~f:(fun ass ->
       let q_filled = check_q_filled ass in
       let q_overlap = check_can_do infos checks l ass in
